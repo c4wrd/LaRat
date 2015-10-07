cd9d5951fa547c2c4870f689190d7ca4f57a30e6
Cory Forward <z41r951@montana.edu>
2015-10-06 22:36:36 -0600

    Added some OpenGL things for future implementation of a nice feature.

--- Android/LaRat/app/src/main/java/com/c4wd/larat/LaRatActivity.java
+++ Android/LaRat/app/src/main/java/com/c4wd/larat/LaRatActivity.java
@@ -2,6 +2,8 @@ package com.c4wd.larat;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.WindowManager;
@@ -14,6 +16,11 @@ public class LaRatActivity extends Activity {
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
 
+        /*GLSurfaceView view = new GLSurfaceView(this);
+        view.setRenderer(new OpenGLRenderer());
+        view.getHolder().setFormat(PixelFormat.TRANSLUCENT);
+        view.setZOrderOnTop(true);
+        setContentView(view);*/
 
         if (!LaRatServiceReceiver.IsServiceRunning(getApplicationContext())) {
             startService(new Intent(getApplicationContext(), LaRatService.class));
---
+++ Android/LaRat/app/src/main/java/com/larat/opengl/OpenGLRenderer.java
@@ -0,0 +1,58 @@
+package com.larat.opengl;
+
+import android.opengl.GLSurfaceView;
+import android.opengl.GLU;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+/**
+ * Created by cory on 10/6/15.
+ */
+
+
+class OpenGLRenderer implements GLSurfaceView.Renderer {
+
+    private Cube mCube = new Cube();
+    private float mCubeRotation;
+
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        gl.glClearColor(0.0f, 0.0f, 0.0f, 0.5f);
+
+        gl.glClearDepthf(1.0f);
+        gl.glEnable(GL10.GL_DEPTH_TEST);
+        gl.glDepthFunc(GL10.GL_LEQUAL);
+
+        gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT,
+                GL10.GL_NICEST);
+
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+        gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+        gl.glLoadIdentity();
+
+        gl.glTranslatef(0.0f, 0.0f, -10.0f);
+        gl.glRotatef(mCubeRotation, 1.0f, 1.0f, 1.0f);
+
+        mCube.draw(gl);
+
+        gl.glLoadIdentity();
+
+        mCubeRotation -= 0.15f;
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        gl.glViewport(0, 0, width, height);
+        gl.glMatrixMode(GL10.GL_PROJECTION);
+        gl.glLoadIdentity();
+        GLU.gluPerspective(gl, 45.0f, (float) width / (float) height, 0.1f, 100.0f);
+        gl.glViewport(0, 0, width, height);
+
+        gl.glMatrixMode(GL10.GL_MODELVIEW);
+        gl.glLoadIdentity();
+    }
+}
