e1ae7d1e6026edc02bb44380d305abbe8d620b8c
Cory Forward <coryf1996@gmail.com>
2015-12-09 07:46:12 -0700

    Updated clients.sql
    A few commits ago I changed from storing SMS threads into their own table, to store them in the message table. This requires a change in the table's structure, just haven't updated this file until now.

--- web_interface/clients.sql
+++ web_interface/clients.sql
@@ -1,32 +1,27 @@
 -- Use this to create the tables needed for LaRat
 
 SET NAMES utf8;
-SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
-SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
-DROP TABLE IF EXISTS `larat_clients`;
-CREATE TABLE `larat_clients` (
-  `objectId` varchar(50) CHARACTER SET utf8 NOT NULL,
+--
+-- Table structure for table `clients`
+--
+CREATE TABLE IF NOT EXISTS `clients` (
+  `objectId` varchar(10) CHARACTER SET utf8 NOT NULL,
   `carrier` varchar(15) CHARACTER SET utf8 NOT NULL,
   `phoneNumber` varchar(10) CHARACTER SET utf8 NOT NULL,
   `deviceid` varchar(25) CHARACTER SET utf8 NOT NULL,
   `sdkversion` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `clients`;
-CREATE TABLE `clients` (
-  `objectId` varchar(50) CHARACTER SET utf8 NOT NULL,
-  `carrier` varchar(15) CHARACTER SET utf8 NOT NULL,
-  `phoneNumber` varchar(10) CHARACTER SET utf8 NOT NULL,
-  `deviceid` varchar(25) CHARACTER SET utf8 NOT NULL,
-  `sdkversion` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
-DROP TABLE IF EXISTS `client_messages`;
-CREATE TABLE `client_messages` (
+--
+-- Table structure for table `client_messages`
+--
+CREATE TABLE IF NOT EXISTS `client_messages` (
   `objectId` varchar(50) CHARACTER SET utf8 NOT NULL,
   `message_type` varchar(50) NOT NULL,
   `message` mediumtext CHARACTER SET utf8 NOT NULL,
@@ -34,14 +29,17 @@ CREATE TABLE `client_messages` (
   `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `location_history`;
-CREATE TABLE `location_history` (
+--
+-- Table structure for table `location_history`
+--
+CREATE TABLE IF NOT EXISTS `location_history` (
   `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `latitude` float NOT NULL,
   `longitude` float NOT NULL,
   `objectId` varchar(10) CHARACTER SET utf8 NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-
--- 2015-10-05 20:55:00
+ALTER TABLE `clients`
+  ADD UNIQUE KEY `objectId` (`objectId`);
